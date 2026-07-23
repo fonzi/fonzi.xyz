6a987ab9e51e2c6065b96e6b0d6e4a02cc76ab4d
Fonzi Vazquez <me@fonzi.xyz>
2026-07-23 16:15:08 -0500

    Adds cache clear

--- public/js/zooming.js
+++ public/js/zooming.js
@@ -6,7 +6,11 @@
   var __getProtoOf = Object.getPrototypeOf;
   var __hasOwnProp = Object.prototype.hasOwnProperty;
   var __commonJS = (cb, mod) => function __require() {
-    return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+    try {
+      return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+    } catch (e) {
+      throw mod = 0, e;
+    }
   };
   var __copyProps = (to, from, except, desc) => {
     if (from && typeof from === "object" || typeof from === "function") {
@@ -25,9 +29,9 @@
     mod
   ));
 
-  // ns-hugo-imp:/home/fonzi/Devel/fonzi.xyz/themes/hugo-theme-monochrome/assets/lib/js/zooming-v2.1.1.min.js
+  // ns-hugo-imp:/home/fonzi/devel/Personal/fonzi.xyz/themes/hugo-theme-monochrome/assets/lib/js/zooming-v2.1.1.min.js
   var require_zooming_v2_1_1_min = __commonJS({
-    "ns-hugo-imp:/home/fonzi/Devel/fonzi.xyz/themes/hugo-theme-monochrome/assets/lib/js/zooming-v2.1.1.min.js"(exports, module) {
+    "ns-hugo-imp:/home/fonzi/devel/Personal/fonzi.xyz/themes/hugo-theme-monochrome/assets/lib/js/zooming-v2.1.1.min.js"(exports, module) {
       !(function(t, e) {
         "object" == typeof exports && "undefined" != typeof module ? module.exports = e() : "function" == typeof define && define.amd ? define(e) : (t = t || self).Zooming = e();
       })(exports, function() {
